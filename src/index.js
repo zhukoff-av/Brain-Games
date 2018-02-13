7843ca259c994ad0896802d69f49eb9c4e88041e
zhukoff-av <mr.zhukoff@gmail.com>
2018-02-13 22:10:38 +0300

    fix eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 // const actual = readlineSync.question('Your answer: ');
 export const sayHello = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
 };
