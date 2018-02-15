89a018a11e7c1f0b58c297fabda5353d85d06266
zhukoff-av <mr.zhukoff@gmail.com>
2018-02-16 00:13:23 +0300

    refactor even

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import { cons } from 'hexlet-pairs';
 import startGame from '..';
 import generateRandom from '../calculation';
 
-const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = num => num % 2 === 0;
 
 const rule = 'Answer "yes" if number even otherwise answer "no".';
 
@@ -12,7 +12,7 @@ export default startPoint;
 
 export const giveQuestionAnswer = () => {
     const question = generateRandom(0, 115);
-    const answer = isEven(question);
+    const answer = isEven(question) ? 'yes' : 'no';
 
     return cons(question, answer);
 };
